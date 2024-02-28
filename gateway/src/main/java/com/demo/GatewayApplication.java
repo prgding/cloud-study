64e293b393b65a59abe90866aea9fa22b9ece925
dings <1203823603@qq.com>
2024-02-28 17:36:33 +0800

    add gateway

---
+++ gateway/src/main/java/com/demo/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dings
+ * @since 2024/2/28 16:40
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
